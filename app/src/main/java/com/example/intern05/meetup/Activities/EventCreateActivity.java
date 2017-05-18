8b95ce289512884d83622d52a51fac6bcf685974
bobi <raul_bobletec@yahoo.com>
2017-05-18 17:52:53 +0300

    Calendar Working

--- app/src/main/java/com/example/intern05/meetup/Activities/EventCreateActivity.java
+++ app/src/main/java/com/example/intern05/meetup/Activities/EventCreateActivity.java
@@ -1,12 +1,15 @@
 package com.example.intern05.meetup.Activities;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.intern05.meetup.R;
 
@@ -14,22 +17,42 @@ public class EventCreateActivity extends AppCompatActivity implements DatePicker
 
 
     Button calendarB;
-    DatePickerDialog datePickerDialog;
+    EditText dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_create);
 
-        datePickerDialog = new DatePickerDialog(getApplicationContext(), this, Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH);
+
         calendarB=(Button)findViewById(R.id.calendarB);
+        dateText=(EditText)findViewById(R.id.dateText2);
         calendarB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                datePickerDialog.show();
+                showDialog(0);
             }
         });
     }
+
+    int year=2017,month=1,day=2;
+
+    @Override
+    @Deprecated
+    protected Dialog onCreateDialog(int id) {
+        return new DatePickerDialog(this, datePickerListener, year, month, day);
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            day = selectedDay;
+            month = selectedMonth;
+            year = selectedYear;
+            dateText.setText(selectedDay + " / " + (selectedMonth + 1) + " / "
+                    + selectedYear);
+        }
+    };
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
