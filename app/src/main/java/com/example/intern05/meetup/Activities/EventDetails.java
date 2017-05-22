b907e415b3bc13f4131b4d77f1c831777f501579
bobi <raul_bobletec@yahoo.com>
2017-05-22 11:56:22 +0300

    EventDetails

--- app/src/main/java/com/example/intern05/meetup/Activities/EventDetails.java
+++ app/src/main/java/com/example/intern05/meetup/Activities/EventDetails.java
@@ -2,14 +2,22 @@ package com.example.intern05.meetup.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.intern05.meetup.R;
 
 public class EventDetails extends AppCompatActivity {
 
+    EditText eventName,eventDate,eventTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        eventName=(EditText)findViewById(R.id.eventDName);
+        eventDate=(EditText)findViewById(R.id.eventDDate);
+        eventTime=(EditText)findViewById(R.id.timeDStart);
+
+
     }
 }
